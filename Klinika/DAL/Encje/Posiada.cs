341f275f9cd69f650d234a1cd97c92849ed0c452
micos <micos@interia.pl>
2021-07-08 23:40:27 +0200

    Połaczenie ze wszystkimi tabelami bazy.

---
+++ Klinika/DAL/Encje/Posiada.cs
@@ -0,0 +1,22 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klinika.DAL.Encje
+{
+    class Posiada
+    {
+        public string Nazwa { get; set; }
+        public uint IdLekarza { get; set; }
+
+        public Posiada(MySqlDataReader reader)
+        {
+            Nazwa = reader["nazwa"].ToString();
+            IdLekarza = uint.Parse(reader["id_l"].ToString());
+        }
+
+    }
+}
---
+++ Klinika/DAL/Repozytoria/RepozytoriumPosiadaja.cs
@@ -0,0 +1,30 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klinika.DAL.Repozytoria
+{
+    using Encje;
+    static class RepozytoriumPosiadaja
+    {
+        private const string WSZYSTKIE_POSIADANIA = "SELECT * FROM posiadaja";
+
+        public static List<Posiada> PobierzWszystkiePosiadania()
+        {
+            List<Posiada> posiadaja = new List<Posiada>();
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand(WSZYSTKIE_POSIADANIA, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                    posiadaja.Add(new Posiada(reader));
+                connection.Close();
+            }
+            return posiadaja;
+        }
+    }
+}
